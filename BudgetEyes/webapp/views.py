53d8f3fe3818d52f67fbed183d25f9410065fc32
Viktor Blidh <vikbl327@student.liu.se>
2020-04-08 01:03:45 +0200

    Added navbar to basepage using bootstrap-vue, and implemented first ajax call with axios library

--- BudgetEyes/webapp/views.py
+++ BudgetEyes/webapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+
 from .forms import CurrencyForm
 from .models import Currency
 
@@ -11,22 +15,32 @@ def index(request):
     if request.method == "POST":
         print("Post")
         form = CurrencyForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            form = CurrencyForm()
-            try:
-                c = Currency.objects.get(name=data.get("currency_name"))
-                c.value = data.get("new_value")
-                c.save()
-            except Currency.DoesNotExist:
-                print("Entry not found")
-        else:
-            print("Not valid")
+        #if form.is_valid(): No longer working due to AJAX call instead of form action
+        data = json.loads(request.body.decode('utf-8'))
+        print(data)
+        form = CurrencyForm()
+        msg = ""
+        try:
+            c = Currency.objects.get(name=data.get("currency_name"))
+            c.value = data.get("new_value")
+            c.save()
+            print("Saved")
+            return HttpResponse("Saved")
+        except Currency.DoesNotExist:
+            print("Entry not found")
+            msg = "Not saved"
+            response = HttpResponse(msg)
+            response.status_code = 400
+            return response
+
+        #else:
+            #print("Not valid")
     else:
         form = CurrencyForm()
 
-    currencies = Currency.objects.all()
-    currlist = [[x.name, x.value] for x in currencies]
-    print(currlist)    
+        currencies = Currency.objects.all()
+        print(currencies)
+        currdict = {k:v for (k,v) in [(x.name, x.value) for x in currencies]}
+        print(currdict)    
 
-    return render(request, 'index.html', {"form":form, "currencies" : currlist})
+        return render(request, 'index.html', {"form":form, "currencies" : currdict})
